078147f152d6e08101cb105335f62ffdbb2da5ed
JordanFitzpatrick <fitzpatrickj1@email.arizona.edu>
2015-09-10 17:00:22 -0700

    Edited test00 to be a simpler starting place

--- testcases/test00.c
+++ testcases/test00.c
@@ -3,6 +3,13 @@
 #include <usloss.h>
 #include <phase1.h>
 
+/* 
+I've edited this for simplicity for now, will uncomment later
+
+start1 doesn't fork any new processes or anything, 
+it just starts up, and quits
+*/
+
 int XXp1(char *);
 char buf[256];
 
@@ -11,12 +18,12 @@ int start1(char *arg)
     int status, pid1, kidpid;
 
     printf("start1(): started\n");
-    pid1 = fork1("XXp1", XXp1, "XXp1", USLOSS_MIN_STACK, 3);
-    printf("start1(): after fork of child %d\n", pid1);
-    printf("start1(): performing join\n");
-    kidpid = join(&status);
-    sprintf(buf,"start1(): exit status for child %d is %d\n", kidpid, status); 
-    printf("%s", buf);
+    //pid1 = fork1("XXp1", XXp1, "XXp1", USLOSS_MIN_STACK, 3);
+    //printf("start1(): after fork of child %d\n", pid1);
+    //printf("start1(): performing join\n");
+    //kidpid = join(&status);
+    //sprintf(buf,"start1(): exit status for child %d is %d\n", kidpid, status); 
+    //printf("%s", buf);
     quit(0);
     return 0; /* so gcc will not complain about its absence... */
 }
